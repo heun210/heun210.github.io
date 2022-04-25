b8e4fbe915b433e414082e8e57c7864f98c79754
heun <heun210@gmail.com>
2022-04-25 11:23:36 +0900

    22.04.25

--- js/weather.js
+++ js/weather.js
@@ -1,4 +1,26 @@
 const API_KEY = "1f033465d9dbba67cc149949ad0406fc";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerHTML = `${data.weather[0].main} / ${data.main.temp}&#176;C`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
+/* const API_KEY = "1f033465d9dbba67cc149949ad0406fc";
 const COORDS = "coords";
 
 const weatherInfo = document.querySelector(".weatherInfo");
@@ -37,4 +59,4 @@ function getWeather(lat, lon) {
     });
 }
 
-askCoords();
+askCoords(); */
